27d7cf1b5fb9ea0168e6418f408e133eef2916ac
qas275 <weishunlim93@hotmail.com>
2022-06-30 14:03:36 +0800

    init commit

---
+++ src/main/java/vttp2022/mainapp/Main.java
@@ -0,0 +1,13 @@
+package vttp2022.mainapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
